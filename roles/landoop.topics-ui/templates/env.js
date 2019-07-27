ef9761e698edd75fcc1ec8d03412bdff085328f0
Benjamin Goldberg <ben@spothero.com>
2019-07-27 12:47:07 -0500

    ansible tweaks

--- roles/landoop.topics-ui/templates/env.js
+++ roles/landoop.topics-ui/templates/env.js
@@ -1,7 +1,7 @@
 var clusters = [
     {
       NAME: "dev",
-      KAFKA_REST: "http://127.0.0.1:8082",
+      KAFKA_REST: "http://{{ hostvars[inventory_hostname]['ansible_env'].SSH_CONNECTION.split(' ')[2] }}:8082",
       MAX_BYTES: "50000",
       RECORD_POLL_TIMEOUT: "3000",
       DEBUG_LOGS_ENABLED: true,
