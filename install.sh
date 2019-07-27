688096b54fa1f2ffee0d4c644a2ef1504531c4f3
Benjamin Goldberg <ben@spothero.com>
2019-07-27 09:56:18 -0500

    install.sh modifications

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-apt-add-repository ppa:ansible/ansible -y
-apt-get update && apt-get install ansible git -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install ansible git -y
 
 
-ansible-galaxy install -r requirements.yml
-#ansible-playbook all.yml -i hosts.yml
+sudo ansible-galaxy install -r requirements.yml
+#sudo ansible-playbook all.yml -i hosts.yml
