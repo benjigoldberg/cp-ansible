7d83528bac03859ea192fc90207a0116cba3dbfe
Benjamin Goldberg <ben@spothero.com>
2019-07-27 12:54:35 -0500

    ansible tweaks

--- roles/landoop.topics-ui/templates/env.js
+++ roles/landoop.topics-ui/templates/env.js
@@ -1,7 +1,7 @@
 var clusters = [
     {
       NAME: "dev",
-      KAFKA_REST: "http://{{ ansible_default_ipv4.address }}:8082",
+      KAFKA_REST: "http://{{ hostvars[inventory_hostname]['ipify_public_ip'] }}:8082",
       MAX_BYTES: "50000",
       RECORD_POLL_TIMEOUT: "3000",
       DEBUG_LOGS_ENABLED: true,
