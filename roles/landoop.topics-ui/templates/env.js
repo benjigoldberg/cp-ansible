8277eab4edefe7e3880ec83fe82b4167fbc53f2d
Benjamin Goldberg <ben@spothero.com>
2019-07-27 12:22:24 -0500

    ansible tweaks

--- roles/landoop.topics-ui/templates/env.js
+++ roles/landoop.topics-ui/templates/env.js
@@ -1,9 +1,8 @@
 var clusters = [
     {
       NAME: "dev",
-      KAFKA_REST: "localhost:8082",
+      KAFKA_REST: "http://127.0.0.1:8082",
       MAX_BYTES: "50000",
-      COLOR: "red",
       RECORD_POLL_TIMEOUT: "3000",
       DEBUG_LOGS_ENABLED: true,
       LAZY_LOAD_TOPIC_META: false
